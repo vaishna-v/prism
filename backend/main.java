c3a8239b187faa5c2733366a288e4c6ee528bd96
vaishna-v <painulyvaishnav132006@gmail.com>
2026-03-23 13:50:10 +0530

    add cli-based menu for backend

--- backend/main.java
+++ backend/main.java
@@ -1,5 +1,5 @@
 import imageEngine.Engine;
-
+import java.util.Scanner;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,11 +7,74 @@ import java.io.IOException;
 
 class Main
 {
+
+
+    public static void CLIPhotoEditor()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to CLI-PHOTO EDITOR");
+
+        // Input an image-
+        BufferedImage image = null;
+        File input = null;
+        while(true) {
+            System.out.print("\t\tPlease provide the path to photo: ");
+            String path = sc.next();
+            try
+            {
+                input = new File(path);
+                image = ImageIO.read(input);
+                break;
+            }
+            catch(IOException e)
+            {
+                System.out.println("Failed to load the image. \n\n ");
+            }
+        }
+
+        System.out.println("\n\t\tImage Loaded succesfully.");
+        int count = 0;
+        boolean startProgram = true;
+        while(startProgram)
+        {
+            String choices = "\n\n\t\t1: Rotate Image\n\t\t2: Transpose Image\n\t\t3: Convert to GrayScale\n\t\t4(default)s: Exit\n\n\t\t\t\tEnter choice: ";
+            System.out.print(choices);
+            int choice = sc.nextInt();
+
+            switch(choice) {
+                case 1:
+                    System.out.println("Rotating Image by 90 degrees");
+                    BufferedImage rotatedImage = Engine.rotate90AntiClockwise(image);
+                    Engine.saveImage(rotatedImage, "png", "output" + count + ".png");
+                    break;
+                case 2:
+                    System.out.println("Transposing Image-");
+                    BufferedImage transposedImage = Engine.transpose(image);
+                    Engine.saveImage(transposedImage, "png", "output" + count + ".png");
+                    break;
+                case 3:
+                    System.out.println("Converting to GrayScale-");
+                    BufferedImage grayScaleImage = Engine.toGreyScale2(image);
+                    Engine.saveImage(grayScaleImage, "png", "output" + count + ".png");
+                    break;
+                default:
+                    startProgram = false;
+        }
+            count++;
+        }
+
+
+
+
+    }
     public static void main(String[] args)
     {
         System.out.println("YES");
 
+        CLIPhotoEditor();
 
+
+        /*
         BufferedImage image = null;
         // for printing new file dimension-
         try {
@@ -42,7 +105,7 @@ class Main
         BufferedImage transposedImage = Engine.transpose(image);
         Engine.saveImage(transposedImage, "png", "images/transposeImage.png");
 
-
+        */
 
     }
 }
